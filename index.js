21aa20d8eb1f9d26f148b8aebe3a8e36cd487689
Lucas Miranda <lucas.alagoinha@gmail.com>
2022-05-08 12:12:35 -0300

    delete method

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-var getJSON = function(url, callback) {
-    var xmlhttprequest = new XMLHttpRequest();
-    xmlhttprequest.open('GET', url, true);
-    xmlhttprequest.responseType = 'json';
-    xmlhttprequest.onload = function() {
-        var status = xmlhttprequest.status;
-        if (status == 200) {
-            callback(null, xmlhttprequest.response);
-        } else {
-            callback(status, xmlhttprequest.response);
-        }
-    };
-    xmlhttprequest.send();
-};
-
-getJSON('http://localhost:8080/customers', function(err, data) {
-    if (err != null) {
-        console.error(err);
-    } else {
-        var display = `ID: ${data.customerID}`;
-    }
-    console.log(display);
-});
+/*function getVal() {
+    var url = 'http://localhost:8080/customers/';
+    var val = document.getElementById('ide').value;
+    url = url + val;
+    console.log(url);
+    const element = document.getElementById('delete-request');
+    fetch(url, { method: 'DELETE' })
+        .then(() => element.innerHTML = 'Delete successful');
+}               
+*/
